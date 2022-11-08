af339948db2270f8a46ad0b22ed1abfd7806c0fd
Jean-Pierre Georgé <george@irit.fr>
2022-11-08 17:19:04 +0100

    test

--- src/histoires/Test.java
+++ src/histoires/Test.java
@@ -2,4 +2,12 @@ package histoires;
 
 public class Test {
 
+	public static void main(String[] args) {
+		System.out.println("BEGIN SAMOURAI 2 \n");
+		
+		
+		
+		System.out.println("\nEND");
+	}
+	
 }
