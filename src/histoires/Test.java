4b8d8264c5970deb4a9d8b3cd2ad43fa67b683b2
Jean-Pierre Georgé <113031570+JPG-IRIT@users.noreply.github.com>
2022-11-08 17:19:57 +0100

    Update Test.java

--- src/histoires/Test.java
+++ src/histoires/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("BEGIN SAMOURAI 2 \n");
 		
-		
+		System.out.println("test");
 		
 		System.out.println("\nEND");
 	}
